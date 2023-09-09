cce648fb7b106e87bb4c001c27771c2561b33aa6
Josh Olson <YoshiOlson@yahoo.com>
2023-09-08 19:42:47 -0600

    makingDecisions2

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -96,7 +96,7 @@ if (score >= 90){
 ////////// PROBLEM 6 //////////
 
 // Do not edit the code below.
-var myFavoriteNumbers = [4,8,12,16,20,24];
+var myFavoriteNumbers = [4,8,12,16,20,24]
 // Do not edit the code above.
 
 /*
@@ -105,14 +105,28 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
+let someNum = myFavoriteNumbers[4]
+{
+  console.log(someNum)
+}
+
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 
+// elements in the array? Let's write some code to check for that. 
+// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this 
+// array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. 
+// (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
 
+if (myFavoriteNumbers.length < 7){
+  console.log('There are not enough elements in this array')
+  }   else if (myFavoriteNumbers.length >= 7){
+    someNum = myFavoriteNumbers[6]
+    console.log(someNum)
+  }
 
 ////////// PROBLEM 8 //////////
 
@@ -120,10 +134,15 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} 
+// is divisible by 3.'
 
 //Code Here
 
+for (let i = 0; i < listOfNumbers.length; i++){
+  if (listOfNumbers[i] % 3 === 0){
+  console.log(listOfNumbers[i] + ' is divisable by 3.')}
+ }
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -136,11 +155,15 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+for (let i = letters.length; i > -1; i--){
+  console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
 ////////// PROBLEM 10 //////////
-// Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
+// Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one 
+//for the following.
 
 // Do not edit the code below.
 let letterGrade = 'B'
